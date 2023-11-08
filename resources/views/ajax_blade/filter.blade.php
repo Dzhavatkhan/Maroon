c14d28c292f5df114cf933f9fd7ff6bcffe312ed
Dzhavatkhan <rp11dzhavatkhanov@gmail.com>
2023-11-08 17:27:31 +0400

    width complete kinda

--- resources/views/ajax_blade/filter.blade.php
+++ resources/views/ajax_blade/filter.blade.php
@@ -1,20 +1,19 @@
 @if ($q > 0)
-@foreach ($products as $pro)
+@foreach ($products as $product)
     <div class="product_card all">
-        <a style="text-decoration: none" href="{{ route('product',$product->product_id) }}"><img src="{{ asset('img/products/'.$pro->image) }}" alt=""></a>
+        <a style="text-decoration: none" href="{{ route('product',$product->product_id) }}"><img src="{{ asset('img/products/'.$product->image) }}" alt=""></a>
         <div class="product_card_name_price">
             <a style="text-decoration: none" href="{{ route('product',$product->product_id) }}">
-                <p class="product_name">{{ $pro->product_name }}</p> 
+                <p class="product_name">{{ $product->product_name }}</p>
             </a>
-            <p class="product_price">{{ $pro->price }}</p>  
+            <p class="product_price">{{ $product->price }}</p>
         </div>
-        <p class="product_category">{{ $pro->category }}</p>  
+        <p class="product_category">{{ $product->category }}</p>
 
 
     </div>
 @endforeach
 @else
     <h2>Результатов: 0</h2>
-    <p>body {{ $body_query }}</p>
 @endif
 
